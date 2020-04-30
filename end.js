7d3f3cf7b58da452fbc376e330991562908624fa
dude613 <liamswift2014@gmail.com>
2020-04-30 15:39:39 -0400

    highscores

--- end.js
+++ end.js
@@ -22,9 +22,7 @@ saveHighScore = e => {
         name: username.value
     };
     highScores.push(score);
-    highScores.sort((a,b) => {
-        return b.score - a.score;
-    });
+    highScores.sort((a,b) => b.score - a.score);
     highScores.splice(5);
 
     localStorage.setItem("highScores", JSON.stringify(highScores));
